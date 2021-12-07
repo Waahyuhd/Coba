cda6a236a3f77d5fca3109da0fa5278bfa7b8c02
Wahyu Hidayat <wahyuhidayat@students.amikom.ac.id>
2021-12-07 21:28:51 +0700

    ssssss

---
+++ WAH/WAH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlasasd!");
+        }
+    }
+}
