b4d5a677fac4e534c626ee5627283e73a66a5444
Wahyu Hidayat <wahyuhidayat@students.amikom.ac.id>
2021-12-07 20:46:17 +0700

    commit1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Coba d git");
         }
     }
 }
